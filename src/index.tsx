276ede34fce0324acf3250d6d1d99f9db87956fd
wonnyfront <101866000+wonnyfront@users.master.github.com>
2023-09-20 22:14:01 +0900

    npm bootswatch

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // style
-import '../common/css/bootstrap.min.css';
+import '../common/css/common.css';
+import 'bootswatch/dist/sketchy//bootstrap.min.css';
 
 const rootNode = document.getElementById('root');
 
